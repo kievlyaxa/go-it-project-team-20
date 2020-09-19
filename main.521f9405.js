6fb7b8991ddd88592f5e597d360acc5003146bbf
Serhii Skybinskyi <serhii.skybinskyi@gmail.com>
2020-09-19 17:42:34 +0300

    Updates

--- main.521f9405.js
+++ main.521f9405.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58653" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57263" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"C:\\Users\\w537QF43\\Documents\\GitHub\\go-it-project-team-20\\src\\images\\png\\black-fire-desktop.png":[["black-fire-desktop.59a38ea6.png","images/png/black-fire-desktop.png"],"images/png/black-fire-desktop.png"],"C:\\Users\\w537QF43\\Documents\\GitHub\\go-it-project-team-20\\src\\images\\png\\our-prog-desktop.png":[["our-prog-desktop.03cb6252.png","images/png/our-prog-desktop.png"],"images/png/our-prog-desktop.png"],"C:\\Users\\w537QF43\\Documents\\GitHub\\go-it-project-team-20\\src\\images\\png\\our-prog-tablet.png":[["our-prog-tablet.d11446f7.png","images/png/our-prog-tablet.png"],"images/png/our-prog-tablet.png"],"C:\\Users\\w537QF43\\Documents\\GitHub\\go-it-project-team-20\\src\\images\\png\\our-prog-mobile.png":[["our-prog-mobile.138890da.png","images/png/our-prog-mobile.png"],"images/png/our-prog-mobile.png"],"./..\\images\\vk.svg":[["vk.3dc341a5.svg","images/vk.svg"],"images/vk.svg"],"./..\\images\\Arrow1.svg":[["Arrow1.e26ec992.svg","images/Arrow1.svg"],"images/Arrow1.svg"],"./..\\images\\Arrow2.svg":[["Arrow2.233ace33.svg","images/Arrow2.svg"],"images/Arrow2.svg"],"C:\\Users\\w537QF43\\Documents\\GitHub\\go-it-project-team-20\\src\\images\\png\\footer-desktop.png":[["footer-desktop.d7514fbf.png","images/png/footer-desktop.png"],"images/png/footer-desktop.png"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"index.js":[function(require,module,exports) {
+},{"C:\\Users\\w537QF43\\Documents\\GitHub\\go-it-project-team-20\\src\\images\\png\\black-fire-desktop.png":[["black-fire-desktop.59a38ea6.png","images/png/black-fire-desktop.png"],"images/png/black-fire-desktop.png"],"C:\\Users\\w537QF43\\Documents\\GitHub\\go-it-project-team-20\\src\\images\\png\\our-prog-desktop.png":[["our-prog-desktop.03cb6252.png","images/png/our-prog-desktop.png"],"images/png/our-prog-desktop.png"],"C:\\Users\\w537QF43\\Documents\\GitHub\\go-it-project-team-20\\src\\images\\png\\our-prog-tablet.png":[["our-prog-tablet.d11446f7.png","images/png/our-prog-tablet.png"],"images/png/our-prog-tablet.png"],"C:\\Users\\w537QF43\\Documents\\GitHub\\go-it-project-team-20\\src\\images\\png\\our-prog-mobile.png":[["our-prog-mobile.138890da.png","images/png/our-prog-mobile.png"],"images/png/our-prog-mobile.png"],"./..\\images\\Arrow1.svg":[["Arrow1.e26ec992.svg","images/Arrow1.svg"],"images/Arrow1.svg"],"./..\\images\\sprite.svg":[["sprite.5ec50489.svg","images/sprite.svg"],"images/sprite.svg"],"C:\\Users\\w537QF43\\Documents\\GitHub\\go-it-project-team-20\\src\\images\\png\\footer-desktop.png":[["footer-desktop.d7514fbf.png","images/png/footer-desktop.png"],"images/png/footer-desktop.png"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"index.js":[function(require,module,exports) {
 "use strict";
 
 require("./sass/main.scss");
@@ -221,7 +221,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58653" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57263" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
